b988fafea4277b78a2f31382f94bc6ef21f9b63a
AaditT <aadit.trivedi03@gmail.com>
2020-05-29 23:13:58 -0700

    Initial Commit

---
+++ WhackMole/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhackMole
+//
+//  Created by Aadit Trivedi on 5/29/20.
+//  Copyright © 2020 Aadit Trivedi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
